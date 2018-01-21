b7243d27c29425c6891b3cb6a0b7fcd462f3551b
Adrysson0 <adrysson_santos@msn.com>
2018-01-21 01:45:10 -0200

    Final commit

--- src/Controller/Component/OpenWeatherMapComponent.php
+++ src/Controller/Component/OpenWeatherMapComponent.php
@@ -3,57 +3,32 @@
 namespace App\Controller\Component;
 
 use Cake\Controller\Component;
-use Cake\I18n\Time;
-use Cake\Core\Configure;
 
 class OpenWeatherMapComponent extends Component
 {
-  
+  private $key;
+
+  function __construct($key)
+  {
+    $this->key = $key;
+  }
   //function to search OpenWeatherMap API data
-  public function request($city, $key = null){
-    $this->_controller = $controller;
-    $url = 'http://api.openweathermap.org/data/2.5/weather?q='.$city.'&appid='.$key;
+  public function request($city){
+    $url = 'http://api.openweathermap.org/data/2.5/weather?q='.$city.'&appid='.$this->key;
     $response = json_decode(file_get_contents($url));
     if($response == null){
       return false;
     }
     $values = array(
-      [
-        'name' => 'Cidade',
-        'value'=> $response->name
-      ],
-      [
-        'name' => 'Temperatura',
-        'value'=> ($response->main->temp-273).' ºC'
-      ],
-      [
-        'name' => 'Temperatura Máxima',
-        'value'=> ($response->main->temp_min-273).' ºC'
-      ],
-      [
-        'name' => 'Temperatura Mínima',
-        'value'=> ($response->main->temp_max-273).' ºC'
-      ],
-      [
-        'name' => 'Umidade',
-        'value'=> $response->main->humidity.' %'
-      ],
-      [
-        'name' => 'Descrição',
-        'value'=> $response->weather[0]->description
-      ],
-      [
-        'name' => 'Nascer do Sol',
-        'value'=> substr(date("Y-m-d H:i:s", $response->sys->sunrise), 11)
-      ],
-      [
-        'name' => 'Pôr do Sol',
-        'value'=> substr(date("Y-m-d H:i:s", $response->sys->sunset), 11)
-      ],
-      [
-        'name' => 'Vento',
-        'value'=> ($response->wind->speed)*3.6 . ' Km/h'
-      ]
+      'Cidade' => $response->name,
+      'Temperatura' => ($response->main->temp-273).' ºC',
+      'Temperatura Máxima' => ($response->main->temp_min-273).' ºC',
+      'Temperatura Mínima' => ($response->main->temp_max-273).' ºC',
+      'Umidade' => $response->main->humidity.' %',
+      'Descrição' => $response->weather[0]->description,
+      'Nascer do Sol' => substr(date("Y-m-d H:i:s", $response->sys->sunrise), 11),
+      'Pôr do Sol' => substr(date("Y-m-d H:i:s", $response->sys->sunset), 11),
+      'Vento' => ($response->wind->speed)*3.6 . ' Km/h'
     );
     return $values;
   }
